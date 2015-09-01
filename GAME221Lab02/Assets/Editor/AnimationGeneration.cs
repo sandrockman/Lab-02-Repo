bb97e6a1057678454960f1ac9fa5a644d65d5bb1
sandrockman <svc-student@RC335G03.student.directory.jccc>
2015-09-01 08:58:54 -0500

    Started animation creation tool
    yay.

---
+++ GAME221Lab02/Assets/Editor/AnimationGeneration.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AnimationGeneration : EditorWindow {
+
+	public static Object selectedObject;
+
+	int numAnimations;
+	string controllerName;
+	string[] animationNames = new string[100];
+	string[] clipFrameRate = new string[100];
+	int[] clipTimeBetween = new int[100];
+	int[] startFrames = new int[100];
+	int[] endFrames = new int[100];
+	bool[] pingPong = new bool[100];
+	bool[] loop = new bool[100];
+
+	[MenuItem("Project Tools/2DAnimations")]
+	public static void Init()
+	{
+		selectedObject = Selection.activeObject;
+
+		if (selectedObject == null)
+			return;
+
+		AnimationGeneration window = (AnimationGeneration)EditorWindow.GetWindow (typeof(AnimationGeneration));
+		window.Show ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
